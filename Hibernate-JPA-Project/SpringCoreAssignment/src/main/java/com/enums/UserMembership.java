7a65b36dbdaa37c8da5dbe252126e614c41d452e
Smit Bhatiya <138124253+SmitBhatiya29@users.noreply.github.com>
2026-03-25 10:26:17 +0530

    Delete Hibernate-JPA-Project directory

--- Hibernate-JPA-Project/SpringCoreAssignment/src/main/java/com/enums/UserMembership.java
+++
@@ -1,5 +0,0 @@
-package com.enums;
-
-public enum UserMembership {
-    Normal,Primum
-}
--- Hibernate-JPA-Project/src/main/java/com/enums/JobTitle.java
+++
@@ -1,7 +0,0 @@
-package com.enums;
-
-public enum JobTitle {
-    CAPTAIN,
-    ASST_CAPTAIN,
-    CREW_MEMBER
-}
